f1c7b18926430ef5931fdbb4476d5f94d7943ddf
kmajchrzak <krzysztof.majchrzak10.km@gmail.com>
2022-06-06 23:30:40 +0200

    almost ready detection

---
+++ intervariants.cpp
@@ -0,0 +1,177 @@
+#include "intervariants.h"
+using namespace std;
+FieldInvariants::FieldInvariants(vector<pair<int, int> >  ff) :field(ff)
+{
+    
+    /*** Invariants calculation ***/
+    c11 = centralM(1,1);
+    c02 = centralM(0,2);
+    c20 = centralM(2,0);
+    c12 = centralM(1,2);
+    c21 = centralM(2,1);
+    c30 = centralM(3,0);
+    c03 = centralM(0,3);
+    M00 = M(0, 0);
+    M10 = M(1, 0);
+    M01 = M(0, 1);
+
+    I1 = c20 + c02; I1 /= pow(M00, 2);
+	I2 = pow(c20 - c02, 2) + 4 * pow(c11, 2); I2 /= pow(M00, 4);
+	I3 = pow(c30 - 3 * c12, 2) + pow(3 * c21 - c03, 2); I3 /= pow(M00, 5);
+	I4 = pow(c30 + c12, 2) + pow(c21 + c03, 2); I4 /= pow(M00, 5);
+	I5 = (c30 - 3 * c12) * (c30 + c12) *
+        (pow(c30 + c12, 2) - 3 * pow(c21 + c03, 2)) + 
+        (3 * c21 - c03) * (c21 + c03) * (3 * pow(c30 + c12, 2) - 
+        pow(c21 + c03, 2)); I5 /= pow(M00, 10);
+	I6 = (c20 - c02) * (pow(c30 + c12, 2) - pow(c21 + c03, 2)) + 
+    4 * c11 * (c30 + c12) * (c21 + c03); I6 /= pow(M00, 7);
+
+    /*** gravity center ***/
+    gc = std::pair<double, double>(M01 / M00, M10 / M00);
+    
+}
+double FieldInvariants::M(int x, int y)
+{
+    double ret = 0;
+	for (auto i = 0; i < field.size(); i++) {
+		ret += pow(field[i].second, y) * pow(field[i].first, y);
+	}
+	return ret;
+}
+double FieldInvariants::centralM(int x, int y)
+{
+    double ret = 0.0, xcenter = M10 / M00, ycenter = M01 / M00;
+	for (auto i = 0; i < field.size(); i++) {
+		ret += pow(field[i].second - xcenter, x) * pow(field[i].first - ycenter, y);
+	}
+	return ret;
+}
+bool FieldInvariants::findCircle() // Pablo check
+{
+    if (  !(I1 < 0.210226 * 0.8 || I1 > 0.252874 * 1.2) 
+       && !(I2 < 0.00012 * 0.8 || I2 >  0.021667 * 1.2)
+       && !(I3 < -0.000001 * 1.2 || I3 > 0.000058 * 1.2)
+       && !(I4 < -0.000001 * 1.2 || I4 > 0.000017 * 1.2)
+       && !(I5 < -0.000001 * 1.2 || I5 > 0.000001 * 1.2)
+       && !(I6 < -0.000001 * 1.2 || I6 > 0.000001 * 1.2)
+    )
+        return true;
+    else
+		return false;
+}
+
+bool FieldInvariants::findD()
+{
+    if (  !(I1 < 0.197702 * 0.8 || I1 > 0.227674 * 1.2) 
+       && !(I2 < 0.000273 * 0.8 || I2 > 0.007914 * 1.2)
+       && !(I3 < 0.00028 * 0.8 || I3 > 0.000478 * 1.2)
+       && !(I4 < -0.000001 * 1.2 || I4 > 0.000012 * 1.2)
+       && !(I5 < -0.000001 * 1.2 || I5 > 0.003797 * 1.2)
+       && !(I6 < -0.000001 * 1.2 || I6 > 0.000001 * 1.2)
+    )
+        return true;
+    else
+		return false;
+}
+
+bool FieldInvariants::findL()
+{
+    if (  !(I1 < 0.232904 * 0.7 || I1 > 0.402047 * 1.2) 
+       && !(I2 < 0.001229 * 0.7 || I2 > 0.109387 * 1.2)
+       && !(I3 < 0.002978 * 0.7 || I3 > 0.01445 * 1.2)
+       && !(I4 < 0.000188 * 0.7 || I4 >0.006359 * 1.2)
+       && !(I5 < -0.000001 * 1.3 || I5 >0.00006 * 1.2)
+       && !(I6 < -0.000068 * 1.3 || I6 >  0.002069 * 1.2)
+    )
+        return true;
+    else
+		return false;
+}
+
+bool FieldInvariants::findI()
+{
+    if (  !(I1 < 0.166381 * 0.8 || I1 > 0.19249 * 1.2) 
+       && !(I2 < 0.000004 * 0.8 || I2 > 0.003012 * 1.2)
+       && !(I3 < 0.00001 * 0.8 || I3 > 0.000852 * 1.2)
+       && !(I4 < -0.000001 * 1.2 || I4 >  0.000053 * 1.2)
+       && !(I5 < -0.000001 * 1.2 || I5 > 0.000001 * 1.2)
+       && !(I6 < -0.000001 * 1.2 || I6 >  0.000001 * 1.2)
+    )
+        return true;
+    else
+		return false;
+}
+
+bool FieldInvariants::findDot()
+{
+    if (  !(I1 < 0.159161 * 0.8 || I1 > 0.195348 * 1.2) 
+       && !(I2 < -0.000001 * 1.2 || I2 > 0.012819 * 1.2)
+       && !(I3 < -0.000001 * 1.2 || I3 > 0.000105 * 1.2)
+       && !(I4 < -0.000001 * 1.2 || I4 >  0.00001 * 1.2)
+       && !(I5 < -0.000001 * 1.2 || I5 > 0.000001 * 1.2)
+       && !(I6 < -0.000001 * 1.2 || I6 > 0.000001 * 1.2)
+    )
+        return true;
+    else
+		return false;
+}
+
+pair<double, double> FieldInvariants::getgc()
+{
+	return gc;
+}
+
+bool FieldInvariants::isgcInBB(FieldInvariants field_)
+{
+	auto g = field_.getgc();
+	return boundigBox.isIn(g);
+}
+// TODO !!!!!!!!!!!!!
+
+double FieldInvariants::rToCenter(FieldInvariants field_)
+{
+	auto fgc = field_.getgc();
+	return sqrt(pow(gc.second - fgc.second, 2) + pow(gc.first - fgc.first, 2));
+}
+
+pair<double, double> FieldInvariants::calculategc()
+{
+	int sumx = 0;
+    int sumy = 0;
+	for (auto x = 0; x < field.size(); x++) {
+		sumx = field[x].second; sumy = field[x].first;
+	}
+	pair<double, double> result(sumy / field.size(), sumx / field.size());
+	return result;
+}
+
+
+
+
+
+
+
+
+/** Bounding Box **/
+BoundingBox::BoundingBox()
+{
+}
+
+bool BoundingBox::isIn(std::pair<int,int> p)
+{
+	if (x_m <= p.second && x_mx >= p.second && y_m <= p.first && y_mx >= p.first)
+		return true;
+	return false;
+}
+
+int BoundingBox::height()
+{
+	return y_mx - y_m + 1;
+}
+
+int BoundingBox::width()
+{
+	return x_mx - x_m + 1;
+}
+
+
--- main.cpp
+++ main.cpp
@@ -3,44 +3,47 @@
 #include <iostream>
 #include <math.h>
 #include "utils.h"
-#include<stack>
+#include <stack>
+#include "intervariants.h"
 
 using namespace std;
 
-std::vector<std::vector<pair<int,int> > > getField(cv::Mat& photo);
+std::vector<std::vector<pair<int, int>>> getField(cv::Mat &photo);
+void sortField(std::vector<std::vector<pair<int, int>>> &field);
 
 /*** Color ranges ***/
-pair yellow1(17,45), yellow2(90,256), yellow3(90,256);
-pair blue1(95,145), blue2(45,256), blue3(0,256);
-pair red_i1(0,14), red_i2(0,256), red_i3(0,256);
-pair red_c1(165,181), red_c2(0,256), red_c3(0,256);
-//yellow1.first = 1
+pair yellow1(17, 45), yellow2(90, 256), yellow3(90, 256);
+pair blue1(95, 145), blue2(45, 256), blue3(0, 256);
+pair red_i1(0, 14), red_i2(0, 256), red_i3(0, 256);
+pair red_c1(165, 181), red_c2(0, 256), red_c3(0, 256);
+// yellow1.first = 1
 
-int main(int, char* []) {
-    while(true) // Main loop
+int main(int, char *[])
+{
+    const int minValidFieldSize = 64;
+    while (true) // Main loop
     {
         /*** Image loading ***/
 
         cv::Mat photo;
         string photo_path;
 
-        while(!photo.data) // Data loading loop
+        while (!photo.data) // Data loading loop
         {
             cout << "Image name:";
             cin >> photo_path;
-            photo =  cv::imread(photo_path);
-            if(photo.data)
+            photo = cv::imread(photo_path);
+            if (photo.data)
             {
                 cout << "Image" + photo_path + "read, starting detection..." << endl;
                 break;
             }
-                
         }
 
         /*** Image correction ***/
-        
+
         cout << "Starting image correction" << endl;
-        
+
         /** Image sharpening using unsharp mask F(x,y) = f(x,y)+k*( f(x,y)-Fblur(x,y) ) **/
 
         cv::Mat unsharpedImage = unsharp(photo);
@@ -55,12 +58,12 @@ int main(int, char* []) {
 
         cout << "Starting equlization on HSV image" << endl;
 
-        cv::Mat equlized = equlize(hsv); 
+        cv::Mat equlized = equlize(hsv);
 
         /*** Logo segmentation ***/
 
         cout << "Starting logo segmentation" << endl;
-        
+
         /** Erosion and dilation filters **/
 
         cv::Mat ym = masking(photo, yellow1, yellow2, yellow3);
@@ -78,7 +81,7 @@ int main(int, char* []) {
         cv::Mat r2m = masking(photo, red_i1, red_i2, red_i3);
         r2m = erosion(r1m);
         r2m = dilation(r1m);
-    
+
         cv::imshow("mask", ym);
         cv::waitKey(0);
 
@@ -89,80 +92,173 @@ int main(int, char* []) {
         auto r1field = getField(r1m);
         auto r2field = getField(r2m);
 
-        // Sort the segments
+        /** Sort the segments **/
 
+        sortField(bfield);
+        sortField(yfield);
+        sortField(r1field);
+        sortField(r2field);
 
-    }   
+        /** Searching for letters **/
+
+        std::vector<PotentialHit> hits;
+
+        for (int i = 0; i < yfield.size(); i++) // Iterate through all yellow fields
+        {
+            if (yfield[i].size() > minValidFieldSize) // Minimum threshold
+            {
+                FieldInvariants fi(yfield[i]); // Calculating Invariants 
+                if (fi.findCircle())
+                {
+                    PotentialHit ph;
+                    ph.background = fi;
+                    hits.push_back(ph);
+                }
+            }
+        }
+
+        for (int i = 0; i < hits.size(); i++)
+        {
+            PotentialHit hit = hits[i];
+            std::vector<FieldInvariants> fieldsWithOutlying;
+            std::vector<FieldInvariants> fieldsWithThird;
+
+            for (int j = 0; j < bfield.size(); j++)
+            {
+                if (bfield[j].size() > minValidFieldSize)
+                {
+                    FieldInvariants fi(bfield[j]);
+                    if (hit.background.isGravityCenterInsideBB(fi))
+                    {
+                        if (fi.findD()())
+                            fieldsWithThird.push_back();
+                        if (fi.findL()())
+                            fieldsWithOutlying.push_back();
+                    }
+                }
+            }
+
+            if (fieldsWithOutlying.size() != 2)
+                break;
+            std::vector<pair<int, int>> outlyingField;
+            for (int j = 0; j < 2; j++)
+                std::copy(fieldsWithOutlying[j].field.begin(), fieldsWithOutlying[j].field.end(), std::back_inserter(outlyingField));
+            FieldInvariants outlyingInvariants(outlyingField);
+            auto separation = fieldsWithOutlying[0].getDistanceToCenter(fieldsWithOutlying[1]);
+            auto widthRatio = separation / outlyingInvariants.boundigBox.getWidth();
+            if (0.7 <= widthRatio && widthRatio <= 0.85)
+            {
+                std::vector<FieldInvariant> copy;
+                std::copy(fieldsWithOutlying.begin(), fieldsWithOutlying.end(), std::back_inserter(copy));
+                hit.outlyingLetters = copy;
+            }
+
+            if (fieldsWithThird.size() != 1)
+                break;
+            if (hit.Circle.isGravityCenterInsideBB(fieldsWithThird[0]))
+            {
+                FieldInvariant thirdLetter(fieldsWithThird[0].field);
+                hit.thirdLetter = thirdLetter;
+            }
+
+            std::vector<FieldInvariant> secondLetterBottom;
+            std::vector<FieldInvariant> secondLetterTop;
+            for (int j = 0; j < r1field.size(); j++)
+            {
+                if (s1.size() > minValidFieldSize)
+                {
+                    FieldInvariant fi(s1);
+                    if (bg.Circle.isGravityCenterInsideBB(fi))
+                    {
+                        if (fi.isLetterI() && outlyingInvariants.isGravityCenterInsideBB(fi))
+                            secondLetterBottom.push_back(fi);
+                        if (fi.isRedDot())
+                            secondLetterTop.push_back(fi);
+                    }
+                }
+            }
+
+            std::vector<pair<int, int>> secondLetterField;
+            std::copy(secondLetterBottom[0].field.begin(), secondLetterBottom[0].field.end(), std::back_inserter(secondLetterField));
+            std::copy(secondLetterTop[0].field.begin(), secondLetterTop[0].field.end(), std::back_inserter(secondLetterField));
+            MyCV::SegmentInfo secondLetterFull(secondLetterField);
+            separation = secondLetterBottom[0].getDistanceToCenter(secondLetterTop[0]);
+            auto heightRatio = separation / secondLetterFull.boundigBox.getHeight();
+            if (heightRatio >= 0.43 && heightRatio <= 0.6)
+            {
+                hit.secondLetterBottom = secondLetterBottom[0];
+                hit.secondLetterTop = secondLetterTop[0];
+            }
+            hits[i] = hit;
+        }
+    }
 }
 
-std::vector<std::vector<pair<int,int> > > getField(cv::Mat& photo)
+std::vector<std::vector<pair<int, int>>> getField(cv::Mat &photo)
 {
     // We need a list of fields which is a list of single pixels
 
-    cv::Mat seen(photo.rows, photo.cols, CV_8U); //8 bits per pixel - 255 values
+    cv::Mat seen(photo.rows, photo.cols, CV_8U); // 8 bits per pixel - 255 values
     cv::Mat_<uchar> _S = seen;
     cv::Mat_<uchar> _P = photo;
 
     int height = photo.rows;
     int width = photo.cols;
 
-    for (int x = 0; x < width; x++) 
+    for (int x = 0; x < width; x++)
     { // For every column...
-		for (int y = 0; y < height; y++) 
+        for (int y = 0; y < height; y++)
         { // ...iterate through every row
-            _S(y,x) = 0; 
+            _S(y, x) = 0;
         }
     }
 
-    std::vector<std::vector<pair<int,int> > > result;
+    std::vector<std::vector<pair<int, int>>> result;
 
-    for (int x = 0; x < photo.cols; x++) 
+    for (int x = 0; x < photo.cols; x++)
     { // For every column...
-		for (int y = 0; y < photo.rows; y++) 
-        { // ...iterate through every row
-            if(_P(y,x) == 0)  // It is not a part of mask
+        for (int y = 0; y < photo.rows; y++)
+        {                      // ...iterate through every row
+            if (_P(y, x) == 0) // It is not a part of mask
                 continue;
 
-            std::vector<pair<int,int> > field;
-            std::stack<pair<int,int> > fieldIterator;
-            fieldIterator.push(pair(y,x));
-            pair<int,int> singlePixel;
+            std::vector<pair<int, int>> field;
+            std::stack<pair<int, int>> fieldIterator;
+            fieldIterator.push(pair(y, x));
+            pair<int, int> singlePixel;
 
-            while(true)
+            while (true)
             {
                 singlePixel = fieldIterator.top();
                 fieldIterator.pop();
 
-                if(_P(singlePixel.first, singlePixel.second) == 255 &&
-                  _S(singlePixel.first, singlePixel.second) != 1)
+                if (_P(singlePixel.first, singlePixel.second) == 255 &&
+                    _S(singlePixel.first, singlePixel.second) != 1)
                 {
                     field.push_back(singlePixel);
                     _S(singlePixel.first, singlePixel.second) = 1;
 
                     // Check every single pixel around
 
-                    if(singlePixel.first + 1 < height)
+                    if (singlePixel.first + 1 < height)
                         fieldIterator.push(pair(singlePixel.first + 1, singlePixel.second));
-                    if(singlePixel.first - 1 >= 0)
+                    if (singlePixel.first - 1 >= 0)
                         fieldIterator.push(pair(singlePixel.first - 1, singlePixel.second));
-                    if(singlePixel.second + 1 < width)
+                    if (singlePixel.second + 1 < width)
                         fieldIterator.push(pair(singlePixel.first, singlePixel.second + 1));
-                    if(singlePixel.second - 1 >= 0)
+                    if (singlePixel.second - 1 >= 0)
                         fieldIterator.push(pair(singlePixel.first, singlePixel.second - 1));
-
-
                 }
             }
             result.push_back(field);
         }
 
-        return result;  
-
-
+        return result;
     }
-
-    
-
-
 }
 
+void sortField(std::vector<std::vector<pair<int, int>>> &field)
+{
+    std::sort(field.begin(), field.end(), [](std::vector<pair<int, int>> &v1, std::vector<pair<int, int>> &v2) -> bool
+              { return v1.size() < v2.size(); });
+}
